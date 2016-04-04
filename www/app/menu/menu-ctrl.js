295fee9607b3571bc47bfcf0d36f46592b08d829
bote795 <nbotell2@gmail.com>
2016-04-04 13:00:27 -0500

    organization lists
    admin and member static list

--- www/app/menu/menu-ctrl.js
+++ www/app/menu/menu-ctrl.js
@@ -4,6 +4,11 @@
 	['$scope','$ionicModal', '$rootScope', menuCtrl ])
 	function menuCtrl($scope,$ionicModal, $rootScope){
 		var vm =this;
+
+		this.orgs = {"admin": ["SHPE"],
+					 "member": ["TAMUHACK"]
+					};
+
 			// Create the login modal that we will use later
 		$ionicModal.fromTemplateUrl('app/login/login.html', {
 			scope: $scope
