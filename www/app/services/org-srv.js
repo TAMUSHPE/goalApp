0b09f31118a7619ddfcf2ec9de969bf350c1d394
bote795 <nbotell2@gmail.com>
2016-04-11 14:30:48 -0500

    add org api
    add actual api call

--- www/app/services/org-srv.js
+++ www/app/services/org-srv.js
@@ -1,13 +1,10 @@
 (function() {
 	'use strict';
-	angular.module('starter').service('OrgSrv',
-	[ '_',OrgSrv ])
-	function OrgSrv(_){
+	angular.module('starter').service('orgSrv',
+	[ '_','orgApiSrv',orgSrv ])
+	function orgSrv(_, orgApiSrv){
 		var vm =this;
-		vm._orgs = [
-		{name:"SHPE", _id: 1, private: false, admin: [1], member: []},
-		{name:"TAMUHACK", _id: 2,private: false, admin: [], member: [1]}
-		]	
+		vm._orgs = orgApiSrv.query();	
 		vm._id = null;
 		this.getAll = function() {
 			return _orgs;
@@ -16,8 +13,10 @@
 			vm.admin=	_.chain(vm._orgs)
 							.map("admin")
 							.flatten()
-							.find(1)
-							.value;
+							.find(function(value){
+								return value == 1;
+							})
+							.value();
 			return vm.admin;
 		};
 		this.getAllMember =function() {
@@ -25,14 +24,17 @@
 							.map("member")
 							.flatten()
 							.find(1)
-							.value;
+							.value();
 			return vm.member;			
 		};
 		this.set = function(id) {
 			vm._id = id;
 		};
 		this.create=function(org){
-			vm._orgs.push(org);
+			orgApiSrv.save(org,function(result) {
+				console.log(result);
+				vm._orgs.push(result);
+			});
 		};
 		this.edit=function(params) {
 			var item = this.get();
