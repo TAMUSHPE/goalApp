257763fdbb47f930c81d1dd16631e651eec95bce
bote795 <nbotell2@gmail.com>
2016-05-08 20:17:14 -0500

    fixed history after on create

--- www/app/organization/newOrg/newOrg-ctrl.js
+++ www/app/organization/newOrg/newOrg-ctrl.js
@@ -1,13 +1,19 @@
 (function() {
 	'use strict';
 	angular.module('starter').controller('newOrgCtrl',
-	[ 'orgSrv', '$state',newOrgCtrl ])
-	function newOrgCtrl(orgSrv,$state){
+	[ 'orgSrv', '$state', '$location', '$ionicHistory',newOrgCtrl ])
+	function newOrgCtrl(orgSrv,$state,$location, $ionicHistory){
 		var vm =this;
 		this.newOrg = function(org){
 			console.log(org);
 			orgSrv.create(org);
-	    	$state.go('app.playlists');
+	    	//$state.go('app.playlists');
+	    	$ionicHistory.nextViewOptions({
+    			historyRoot: true
+			});
+
+			// Go back to home
+			$location.path('/app/playlists');
 		}
 	}
 })();
